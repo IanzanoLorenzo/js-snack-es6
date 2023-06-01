5fc2bb0c84b7813eafc24f42afb334a818427422
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-06-01 13:25:14 +0200

    invitati done

--- invitati/script/script.js
+++ invitati/script/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const tavoloVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+let segnaposto = tavoloVip.map((item, i) => {
+    let invitato = {
+        "Nome Tavolo":"Tavolo Vip",
+        "Nome dell'ospite": item,
+        "Posto Occupato":i + 1
+    };
+    return invitato;
+} 
+)
+
+console.log(segnaposto)
