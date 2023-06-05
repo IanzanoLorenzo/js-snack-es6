7feb8d2ca7a66cbfa6551f3f449db8e9f55d458e
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-06-05 17:26:45 +0200

    snack 4 done

--- squadre/script/script.js
+++ squadre/script/script.js
@@ -1,1 +1,56 @@
 "use strict";
+
+let squadre = [
+    {
+        nome    :'Juventus' 
+    },
+    {
+        nome    :'Foggia' 
+    },
+    {
+        nome    :'Inter' 
+    },
+    {
+        nome    :'Milan' 
+    },
+    {
+        nome    :'Napoli' 
+    },
+    {
+        nome    :'Sampdoria' 
+    },
+    {
+        nome    :'Pulcini di Villar Basso' 
+    },
+    {
+        nome    :'Tom' 
+    },
+    {
+        nome    :'Boh FC' 
+    },    
+    {
+        nome    :'SC Non Conosco Squadre' 
+    },
+    {
+        nome    :'Aspe\' Forse Una FC' 
+    },
+    {
+        nome    :'La Grande Torino (ma quello bello no quello di ora)' 
+    }    
+]
+
+function randomNumber(min,max){ 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+squadre.forEach((squadra) => {
+    squadra.puntiFatti = randomNumber(1, 10)
+    squadra.falliSubiti = randomNumber(1, 10)
+})
+
+let newSquadreArray = squadre.map((squadra) => {
+    const {nome, falliSubiti} = squadra
+    return {nome, falliSubiti}
+})
+
+console.log(newSquadreArray)
