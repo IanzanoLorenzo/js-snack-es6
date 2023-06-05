827229c7b0e48c7b3eb35b50091114c1cc10e772
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-06-05 17:00:16 +0200

    snack 3 done

--- bici/script/script.js
+++ bici/script/script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+let biciclette = [
+    {
+        nome : 'Bici 1',
+        peso : '8.6 kg'
+    },
+    {
+        nome : 'Bici 2',
+        peso : '7.5 kg'
+    },
+    {
+        nome : 'Bici 3',
+        peso : '6.8 kg'
+    },
+    {
+        nome : 'Bici 4',
+        peso : '8.2 kg'
+    },
+    {
+        nome : 'Bici 5',
+        peso : '9.1 kg'
+    },
+    {
+        nome : 'Bici 6',
+        peso : '7 kg'
+    }
+]
+
+let pesoBiciLeggera = 0, biciLeggera;
+
+biciclette.forEach((bici) => {
+    let {peso} = bici
+    if(pesoBiciLeggera === 0 || pesoBiciLeggera > peso){
+        pesoBiciLeggera = peso;
+        biciLeggera = bici
+    }
+})
+
+console.log(biciLeggera)
